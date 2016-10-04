d4c90763fe28c5bdc321249bb6effd168db83360
Sean Dennis <denn0134@gmail.com>
2016-10-04 11:16:02 -0500

    Remnove GrinderBlock and Timer

--- src/main/java/com/pfeffamonster/culinarycraft/TileEntities/GrinderTileEntity.java
+++
@@ -1,9 +0,0 @@
-package com.pfeffamonster.culinarycraft.TileEntities;
-
-import net.minecraft.tileentity.TileEntity;
-
-/**
- * Created by spdennis on 3/12/2015.
- */
-public class GrinderTileEntity extends TileEntity {
-}
